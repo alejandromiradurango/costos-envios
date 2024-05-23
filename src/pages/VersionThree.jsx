14cdb5f10dba28b0da530157d95f44f54e756cc3
Alejandro Mira Durango <89473354+alejandromiradurango@users.noreply.github.com>
2024-05-23 12:06:06 -0500

    Update version 3

--- src/pages/VersionThree.jsx
+++ src/pages/VersionThree.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
-import { countries, inputs } from '../constants'
+import React, { useEffect, useRef, useState } from 'react'
+import { countries, inputs, precios } from '../constants'
 import { calcularCostoEnvio } from '../utils'
 
 // Custom hook para manejar el estado de los productos
 const useProducts = (initialInputs) => {
   const createProduct = () => {
-    const initialProduct = { id: Date.now() }
+    const initialProduct = { id: Date.now(), cantidad: 1 }
     initialInputs.forEach(input => {
       initialProduct[input.id] = ''
     })
@@ -40,21 +40,35 @@ const VersionThree = () => {
   const { products, handleChange, addProduct, removeProduct } = useProducts(inputs)
   const [customerSelected, setCustomerSelected] = useState(0)
   const [total, setTotal] = useState(null)
+  const [zoneSelected, setZoneSelected] = useState('')
+  const formRef = useRef(null)
+  console.log(formRef.current)
 
   const customers = [0, 30, 50, 70, 100]
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    if (e) e.preventDefault()
 
-    const country = e.target.destination.value
+    const form = e ? e.target : formRef.current
+
+    const country = form.destination.value
     const zone = countries.find(({ name }) => name === country).zone
+    setZoneSelected(zone)
     const totalShip = products.reduce((acc, product) => {
-      return acc + calcularCostoEnvio(product.alto, product.ancho, product.largo, product.peso, zone)
+      return acc + calcularCostoEnvio(product.alto, product.ancho, product.largo, product.peso, zone) * product.cantidad
     }, 0)
 
     setTotal(totalShip)
   }
 
+  const maxKey = Math.max(...Object.keys(precios[zoneSelected] || 'A').map(Number))
+
+  useEffect(() => {
+    if (products.length > 1 && formRef.current) {
+      const submitEvent = new Event('submit', { bubbles: true, cancelable: true })
+      formRef.current.dispatchEvent(submitEvent)
+    }
+  }, [products.length])
   return (
     <div>
       <ul className='flex items-center gap-2 mb-6'>
@@ -64,14 +78,14 @@ const VersionThree = () => {
           </li>
         ))}
       </ul>
-      <div className="max-w-md mx-auto space-y-6 p-6 bg-white rounded-lg shadow-md border">
+      <div className="max-w-2xl mx-auto space-y-6 p-6 bg-white rounded-lg shadow-md border">
         <div className="space-y-2">
           <h1 className="text-2xl font-bold">Calculadora de Costos de Envío</h1>
           <p className="text-gray-500 dark:text-gray-400">
             Ingresa los detalles de tu envío y obtén una estimación del costo.
           </p>
         </div>
-        <form className="space-y-4" onSubmit={handleSubmit}>
+        <form ref={formRef} className="space-y-4" onSubmit={handleSubmit}>
           <div className="space-y-2">
             <label
               className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -91,9 +105,27 @@ const VersionThree = () => {
             </select>
           </div>
           {products.map(product => (
-            <div key={product.id} className="grid grid-cols-3 gap-4 mb-4">
-              {inputs.map(({ id, label }) => (
-                <div className="space-y-2" key={id}>
+            <div key={product.id} className="grid grid-cols-12 mb-4">
+              <div className="space-y-2 col-span-2 self-end">
+                <label
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  htmlFor={`cantidad-${product.id}`}
+                >
+                  Cantidad
+                </label>
+                <select
+                  className="bg-white flex h-10 w-full border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+                  id={`cantidad-${product.id}`}
+                  value={product.cantidad}
+                  onChange={(e) => handleChange(product.id, 'cantidad', Number(e.target.value))}
+                >
+                  {[...Array(20).keys()].map(num => (
+                    <option key={num + 1} value={num + 1}>{num + 1}</option>
+                  ))}
+                </select>
+              </div>
+              {inputs.sort((a, b) => b.id === 'peso').map(({ id, label }) => (
+                <div className="space-y-2 col-span-2" key={id}>
                   <label
                     className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                     htmlFor={`${id}-${product.id}`}
@@ -101,7 +133,7 @@ const VersionThree = () => {
                     {label}
                   </label>
                   <input
-                    className="placeholder:capitalize flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                    className="placeholder:capitalize flex h-10 w-full border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
                     type="number"
                     step="any"
                     id={`${id}-${product.id}`}
@@ -111,13 +143,15 @@ const VersionThree = () => {
                   />
                 </div>
               ))}
-              <button type="button" onClick={() => removeProduct(product.id)} className="self-end text-red-600 border border-red-600 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-3 py-2">
-                Eliminar
-              </button>
+              {products.length > 1 && (
+                <button type="button" onClick={() => removeProduct(product.id)} className="self-end col-span-2 ml-2 text-red-600 border border-red-600 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-3 py-2">
+                  Eliminar
+                </button>
+              )}
             </div>
           ))}
-          <button type="button" onClick={addProduct} className="bg-blue-600 text-white inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-4 py-2 w-full mb-4">
-            Agregar Producto
+          <button type="button" onClick={addProduct} className="text-blue-600 text-xs inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 mb-4">
+            + AGREGUE OTRO PAQUETE
           </button>
           <button className="bg-neutral-800 text-white inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full">
             Calcular Costo
@@ -126,7 +160,7 @@ const VersionThree = () => {
         {total !== null && (
           <div className="bg-gray-100 border p-4 rounded-lg">
             <h2 className="text-lg font-bold">Estimación del Costo de Envío</h2>
-            <p className="text-4xl font-bold text-primary">{!isNaN(total) && '$'}{isNaN(total) ? total : (total + (total * (customerSelected / 100))).toFixed(2)}</p>
+            <p className="text-4xl font-bold text-primary">{!isNaN(total) && '$'}{isNaN(total) ? `Alguno de los paquetes sobrepaso nuestro peso permitido: ${maxKey} kg` : (total + (total * (customerSelected / 100))).toFixed(2)}</p>
           </div>
         )}
       </div>
